e9de65e6d595a86e1048d7c2d2ecaece6bef2d36
yosefBP <juan_karlos_999@hotmail.com>
2021-10-11 17:38:30 -0500

    [Update] login.html, app.py and forms.py

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from forms import LoginForm
+import os
+import yagmail as yag
 
 app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def login():
+    if request.method == 'GET':
+        loginSession = LoginForm()
+        return render_template('login.html', form=loginSession)
+    else:
+        formRequest = LoginForm(request.form)
+        if formRequest.validate_on_submit() == True and formRequest.e_mail.data == 'yosef@gmail.com' :
+            return dashboardSuperAdmin()
+        elif formRequest.validate_on_submit() == True:
+            return '<h1>Success</h1>'
+        else:
+            return '<h1>Failed</h1>'
+
+@app.route('/administrador')
+def dashboardSuperAdmin():
     return render_template('dashboard.html')
--- forms.py
+++ forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from flask_wtf.recaptcha import validators
-from wtforms.fields.core import StringField, PasswordField
-from wtforms.fields.html5 import EmailField
+from wtforms.fields import StringField, PasswordField
+from wtforms.fields.html5 import EmailField, URLField
 from wtforms.fields.simple import SubmitField
-from wtforms.widgets.core import SubmitInput
+from wtforms.widgets.core import CheckboxInput, SubmitInput
+from wtforms import validators
 
 
 class LoginForm(FlaskForm):
-    e_mail = EmailField('inputEmail', validators=[validators.required()])
-    password = PasswordField('inputPassword', validators=[validators.required()])
-    btnLogin = SubmitField('Login')
+    e_mail = EmailField(validators=[validators.required()])
+    password = PasswordField(validators=[validators.required()])
+    checkbox = StringField(widget=CheckboxInput())
+    submit = SubmitField()
