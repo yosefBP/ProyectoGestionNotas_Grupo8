42aae3a45352e2e29592a40681134b3c4c8bd046
yosefBP <juan_karlos_999@hotmail.com>
2021-10-24 23:13:33 -0500

    [ADD] functions validation login and encryption hash password

--- modelos/classModels.py
+++ modelos/classModels.py
@@ -1,7 +1,9 @@
 # sql statement handler class
 
+from re import split
 import modelos.base_models as bm
 from typing import Any
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class Usuarios():
@@ -34,14 +36,16 @@ class Usuarios():
              getRow[0]['telefonoUsuario'], getRow[0]['direccionUsuario'], getRow[0]['password'], getRow[0]['rol_id'])
 
     def insertarUsuario(self):
+        hashed_pwd = generate_password_hash(self.password, method='pbkdf2:sha256',salt_length=32)
         numFilas = bm.insertDb("INSERT INTO Usuarios (idUsuario, nombreUsuario, apellidoUsuario, correoUsuario, telefonoUsuario, direccionUsuario, password, rol_id ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-        [self.idUsuario, self.nombreUsuario, self.apellidoUsuario, self.correoUsuario, self.telefonoUsuario, self.direccionUsuario, self.password, self.rol_id])
+        [self.idUsuario, self.nombreUsuario, self.apellidoUsuario, self.correoUsuario, self.telefonoUsuario, self.direccionUsuario, hashed_pwd, self.rol_id])
 
         return (numFilas > 0)
 
     def actualizarUsuario(self):
+        hashed_pwd = generate_password_hash(self.password, method='pbkdf2:sha256',salt_length=32)
         sql = "UPDATE Usuarios SET nombreUsuario = ?, apellidoUsuario = ?, correoUsuario = ?, telefonoUsuario = ?, direccionUsuario = ?, password = ?, rol_id = ? WHERE idUsuario = ?"
-        afectadas = bm.insertDb(sql, [self.nombreUsuario, self.apellidoUsuario, self.correoUsuario, self.telefonoUsuario, self.direccionUsuario, self.password, self.rol_id, self.idUsuario])
+        afectadas = bm.insertDb(sql, [self.nombreUsuario, self.apellidoUsuario, self.correoUsuario, self.telefonoUsuario, self.direccionUsuario, hashed_pwd, self.rol_id, self.idUsuario])
         return (afectadas >0)
 
     def eliminarUsuario(self):
@@ -52,3 +56,20 @@ class Usuarios():
     @staticmethod
     def get_all():
         return bm.selectDb("SELECT * FROM Usuarios", None)
+
+    @staticmethod
+    def verificarUsuario(correoUsuario, password):
+        getRow = bm.selectDb("SELECT * FROM Usuarios WHERE correoUsuario = ?", [correoUsuario])
+
+        if getRow and len(getRow) == 1:
+            if check_password_hash(getRow[0]['password'], password):
+                return ['True', getRow[0]['rol_id']]
+
+        return ['False']
+
+    @staticmethod
+    def charValidatorPassword(password):
+        for caracter in password:
+            if caracter == "'" or caracter == '"' or caracter == " ":
+                return True
+        return False
